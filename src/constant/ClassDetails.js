89d651cdc25d6a421e87aed81b987a7b60ecc193
liuyu6y <944810294@qq.com>
2021-06-29 18:04:13 +0800

    add WdfMmt

--- src/constant/ClassDetails.js
+++ src/constant/ClassDetails.js
@@ -8,13 +8,35 @@ import {
 } from '@ant-design/icons';
 
 const ClassDetails = [{
-    name: '人脸图片检测',
-
+    name: '人物',
 }, {
-    name: '近期历史记录',
+    name: '食物',
 },{
-    name: '个人帐号管理',
-
+    name: '动物',
+},{
+    name: '植物',
+},{
+    name: '自然',
+},{
+    name: '交通工具',
+},{
+    name: '建筑',
+},{
+    name: '乐器',
+},{
+    name: '服饰箱包',
+},{
+    name: '军事装备',
+},{
+    name: '运动',
+},{
+    name: '器械',
+},{
+    name: '娱乐',
+},{
+    name: '医药用品',
+},{
+    name: '其他',
 },]
 
 export default ClassDetails
---
+++ src/containers/WdfMmtContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import WdfMmt from "../components/WdfMmt";
+
+
+const WdfMmtContainer = () => (
+    <WdfMmt/>
+)
+
+export default WdfMmtContainer;
